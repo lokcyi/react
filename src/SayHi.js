2adb2028bc2f97863ace9fab384f516def497e22
lokcyi <lokcyi@gmail.com>
2019-10-13 12:47:21 +0800

    20191013

--- src/SayHi.js
+++ src/SayHi.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class SayHi extends React.Component {
   constructor(props) {
     super(props);
-    console.log('constructor');
-    //this.handleClick = this.handleClick.bind(this);
+    console.log('url:'+props.match.url);
+    console.log(this.props.location.state.message);
     this.state = {date: new Date()};
   }
   
--- src/index.js
+++ src/index.js
@@ -1,10 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SayHi from './SayHi';
+import AlarmClockControl from './AlarmClockControl';
+import Form from './Form';
+import FormRef from './FormRef';
+import CssStyleTest from './CssStyleTest';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+  } from 'react-router-dom'
+import * as serviceWorker from './serviceWorker';
+//import registerServiceWorker from './registerServiceWorker';
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<Form />, document.getElementById('root'));
+// ReactDOM.render(<FormRef />, document.getElementById('root'));
+ReactDOM.render(<CssStyleTest />, document.getElementById('root'));
+// const Repos = () => (
+//     <div>
+//       <h2>Repos</h2>
+//     </div>
+//   )
+
+  
+//   const About = () => (
+//     <div>
+//       <h2>About</h2>
+//     </div>
+//   )
+//   ReactDOM.render(
+//     <Router>
+//       <div>
+//         <ul>
+//           <li><Link to="/">App</Link></li>
+//           <li><Link to="/repos">Repos</Link></li>
+         
+//           <li><Link to="/about">About</Link></li>
+//           <li>
+//             <Link to={{
+//               pathname: '/sayhi',
+//               state: { id: '1', message: 'hello, component' }
+//             }}>SayHi</Link>
+//           </li>
+//           <li>
+//             <Link to={{
+//               pathname: '/alarmClockControl',
+//             //   state: { id: '1', message: 'hello, component' }
+//             }}>Alarm</Link>
+//           </li>
+//         </ul>
+//         <hr/>
+//         <Route exact path="/" component={App}/>
+//         <Route path="/repos" component={Repos}/>
+//         <Route path="/about" component={About}/>
+//         <Route path="/sayhi" render={props =><SayHi name="joe" {...props} />} />
+//         <Route path="/alarmClockControl" render={props =><AlarmClockControl />} />
+//       </div>
+//     </Router>,
+//      document.getElementById('root'));
+//  // registerServiceWorker();
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
